5dc943df78d0f97bc26c4effafe9fce200e1cb3b
xt18393 <ytdxxt10@163.com>
2019-09-02 15:52:54 +0800

    模板语法 循环 判断 继承

--- app/routes.py
+++ app/routes.py
@@ -10,12 +10,27 @@ from app import app
  Flask将调用该函数并将其返回值作为响应传递回浏览器。这样做是为了在运行这个应用程序的时候会稍微有一点点意义。
 '''
 
+'''
+将模板转换为完整的HTML页面的操作称为渲染。 为了渲染模板，需要从Flask框架中导入一个名为render_template()的函数。 
+该函数需要传入模板文件名和模板参数的变量列表，并返回模板中所有占位符都用实际变量值替换后的字符串结果。
+render_template()函数调用Flask框架原生依赖的Jinja2模板引擎。 Jinja2用render_template()函数传入的参数中的相应值替换{{...}}块
+'''
 @app.route('/')
 @app.route('/index')
 
 def index():
-    user = {'username': 'Terry'}
-    return render_template('index.html', title='home', user=user)
+    user = {'username': 'Terry--'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='home', user=user, posts=posts)
 
 
 #要完成应用程序，你需要在定义Flask应用程序实例的顶层（译者注：也就是microblog目录下）创建一个命名为microblog.py的Python脚本
