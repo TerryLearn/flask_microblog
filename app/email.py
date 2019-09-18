7c5ea7e0629f9aac5313416319bd14dafff0d6db
xt18393 <ytdxxt10@163.com>
2019-09-18 15:24:24 +0800

    修改报错

--- app/email.py
+++ app/email.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from flask_mail import Message
-from app import mail
+from app import mail,app
 from threading import Thread
 
 def send_async_email(app, msg):
@@ -11,7 +11,9 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
+    Thread(target=send_async_email,args=(app,msg)).start()
     mail.send(msg)
+
 
 
 def send_password_reset_email(user):
