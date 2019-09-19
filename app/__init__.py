1be2fa407ecee99cab4a2b40bc9308b22945ab9f
xt18393 <ytdxxt10@163.com>
2019-09-19 10:41:53 +0800

    日期时间flask_moment moment.js的使用

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from logging.handlers import SMTPHandler,RotatingFileHandler
 import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 
 app = Flask(__name__)
@@ -21,6 +22,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 '''
 
 '''
