b89c671b413ed2109656bd3f7ac155c8ebb09e66
xt18393 <ytdxxt10@163.com>
2019-09-29 17:09:11 +0800

    单个用户和用户集合的检索

---
+++ app/api/tokens.py
@@ -0,0 +1,9 @@
+
+
+def get_token():
+    pass
+
+
+def revoke_token():
+    pass
+
--- app/models.py
+++ app/models.py
@@ -6,8 +6,31 @@ from hashlib import md5
 from time import time
 import jwt
 from app import db,login
-from flask import current_app
+from flask import current_app, url_for
 from app.search import add_to_index, remove_from_index, query_index
+
+
+class PaginatedAPIMixin(object):
+    @staticmethod
+    def to_collection_dict(query, page, per_page, endpoint, **kwargs):
+        resources = query.paginate(page, per_page, False)
+        data = {
+            'item':[item.to_dict() for item in resources.items],
+            '_meta': {
+                'page':page,
+                'per_page': per_page,
+                'total_pages': resources.pages,
+                'total_items':resources.total
+            },
+            '_links':{
+                'self': url_for(endpoint, page=page, per_page=per_page, **kwargs),
+                'next': url_for(endpoint, page=page + 1, per_page=per_page,
+                                **kwargs) if resources.has_next else None,
+                'prev': url_for(endpoint, page=page-1, per_page=per_page,
+                                **kwargs) if resources.has_prev else None
+            }
+        }
+        return data
 
 
 class SearchableMixin(object):
@@ -73,7 +96,8 @@ followers = db.Table('followers',
                      db.Column('follower_id', db.Integer, db.ForeignKey('user.id'))
                      )
 
-class User(UserMixin,db.Model):
+
+class User(PaginatedAPIMixin,UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64),index=True, unique=True)
     email = db.Column(db.String(120),index=True, unique=True)
@@ -128,6 +152,32 @@ class User(UserMixin,db.Model):
             return
         return User.query.get(id)
 
+    def to_dict(self, include_email=False):
+        data = {
+            'id':self.id,
+            'username': self.username,
+            'last_seen': (self.last_seen.isoformat() + 'Z') if self.last_seen else None,
+            'about_me': self.about_me,
+            'post_counts': self.posts.count(),
+            'follower_count': self.followers.count(),
+            'followed_count': self.followed.count(),
+            '_links': {
+                'self': url_for('api.get_user', id=self.id),
+                'followers': url_for('api.get_followers', id=self.id),
+                'followed': url_for('api.get_followed', id=self.id),
+                'avatar': self.avatar(128)
+            }
+        }
+        if include_email:
+            data['email'] = self.email
+        return data
+
+    def from_dict(self, data, new_user=False):
+        for field in ['username', 'email', 'about_me']:
+            if field in data:
+                setattr(self, field, data[field])
+            if new_user and 'password' in data:
+                self.set_password(data['password'])
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
